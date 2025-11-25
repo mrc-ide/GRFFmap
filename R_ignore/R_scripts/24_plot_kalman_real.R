5d38449a5296577b9bec71e905cbcb1a2d258a55
cecile-meier-scherling <cecile_meier-scherling@brown.edu>
2025-11-25 20:17:53 +0100

    plot confidence intervals

--- R_ignore/R_scripts/24_plot_kalman_real.R
+++ R_ignore/R_scripts/24_plot_kalman_real.R
@@ -108,22 +108,29 @@ jitter_S <- 1e-10       # diagonal jitter for innovation covariance
 # prediction parameters
 nx <- 200
 ny <- 200
-t_vec <- 2012:2023
+t_vec <- 1995:2025
 t_num <- length(t_vec)
 
 # plotting parameters
-plot_times <- seq(2013, 2023, by = 2) # should be within t_vec
+plot_times <- seq(2012, 2023, by = 1) # should be within t_vec
 t_win <- 2           # window around each facet time (years). Points within this window are displayed
 
 # --------------------------- Load & filter data ----------------------------
 CACHE_DIR <- "R_ignore/R_scripts/outputs/GRFF_kalman_cache_annual_2012_2025"
 dir_create(CACHE_DIR)
-OUT_PREV_DIR   <- file.path("prev_prediction_avg_2_year")
-# OUT_PREV_DIR_LO_UP <- file.path("prev_prediction_grouped_year/upper_lower_prev")
+
+OUT_DIR <- "R_ignore/R_scripts/outputs/plots/key_k13_mutations"
+dir_create(OUT_DIR)
+
+OUT_MEAN_DIR   <- file.path("mean_prev_prediction_avg_2_year")
+OUT_MEDIAN_DIR   <- file.path("median_prev_prediction_avg_2_year")
+OUT_CI_DIR   <- file.path("CI_diff_prediction_avg_2_year")
 OUT_EXCEED_DIR <- file.path("exceedance_prob_avg_2_year")
 
-dir_create(c(paste0("R_ignore/R_scripts/outputs/plots/", OUT_PREV_DIR), 
-             paste0("R_ignore/R_scripts/outputs/plots/", OUT_EXCEED_DIR)),
+dir_create(c(paste0("R_ignore/R_scripts/outputs/plots/key_k13_mutations/", OUT_MEAN_DIR),
+             paste0("R_ignore/R_scripts/outputs/plots/key_k13_mutations/", OUT_MEDIAN_DIR),
+             paste0("R_ignore/R_scripts/outputs/plots/key_k13_mutations/", OUT_CI_DIR),
+             paste0("R_ignore/R_scripts/outputs/plots/key_k13_mutations/", OUT_EXCEED_DIR)),
            recurse = TRUE)
 
 dat <- read.csv("R_ignore/R_scripts/data/all_who_get_prevalence.csv") |>
@@ -195,6 +202,7 @@ pp_cols<- c(
 
 pp_vals <- rescale(c(0, 1, 5, 10, 20, 30, 50, 70, 90, 95 , 100))
 # --- Loop over each mutation ------------------------------
+mut <- "k13:675:V"
 for (mut in all_who_mutations){
   clean_mut <- paste0("k13 ", gsub("^k13:(\\d+):([A-Za-z])$", "\\1\\2", mut))
   
@@ -213,16 +221,13 @@ for (mut in all_who_mutations){
   if (mut == "k13:675:V"){
     xlim = c(28, 37)
     ylim = c(-4, 5)
-  }
-  else if (mut == "k13:comb"){
+  } else if (mut == "k13:comb"){
     xlim = c(28, 48)
     ylim = c(-4.60, 18)
-  }
-  else if (mut == "k13:622:I"){
+  } else if (mut == "k13:622:I"){
     xlim = c(32, 45)
     ylim = c(4, 17)
-  }
-  else if (mut == "k13:561:H"){
+  } else if (mut == "k13:561:H"){
     xlim = c(28, 32)
     ylim = c(-4, 0)
   }
@@ -286,8 +291,10 @@ for (mut in all_who_mutations){
   # --- Build long data for lower / mean / upper ------------------------------
   cached <- make_or_load(mut, ell_km, tau2)
   
-  p_post_median <- cached$p_post_median
+  dim <- cached$p_post_median
   p_post_mean <- cached$p_post_mean
+  p_post_median <- cached$p_post_median
+  p_post_CI <- cached$p_post_CI
   
   xs <- cached$xs
   ys <- cached$ys
@@ -298,16 +305,18 @@ for (mut in all_who_mutations){
   exceed_prob_10 <- exceed_prob[ , , , 3]
   
   # --- Build long data for lower / mean / upper ------------------------------
-  p_long_median <- make_raster_long(p_post_median, xs, ys, plot_times)
-  p_long_mean <- make_raster_long(p_post_mean, xs, ys, plot_times)
+  p_long_median <- make_raster_long(p_post_median, xs, ys, t_vec)
+  p_long_mean <- make_raster_long(p_post_mean, xs, ys, t_vec)
+  p_long_CI <- make_raster_long(p_post_CI, xs, ys, t_vec)
   
-  exceed_prob_long_10 <- make_raster_long(exceed_prob_10, xs, ys, plot_times)
-  exceed_prob_long_5 <- make_raster_long(exceed_prob_5, xs, ys, plot_times)
-  exceed_prob_long_1 <- make_raster_long(exceed_prob_1, xs, ys, plot_times)
+  exceed_prob_long_10 <- make_raster_long(exceed_prob_10, xs, ys, t_vec)
+  exceed_prob_long_5 <- make_raster_long(exceed_prob_5, xs, ys, t_vec)
+  exceed_prob_long_1 <- make_raster_long(exceed_prob_1, xs, ys, t_vec)
   
   # --- Mask out sea as white background ------------------------------
   p_long_median <- mask_to_africa(p_long_median, africa_mask)
   p_long_mean <- mask_to_africa(p_long_mean, africa_mask)
+  p_long_CI <- mask_to_africa(p_long_CI, africa_mask)
   
   exceed_probdraws_long_10 <- mask_to_africa(exceed_prob_long_10, africa_mask)
   exceed_prob_long_5  <- mask_to_africa(exceed_prob_long_5, africa_mask)
@@ -316,6 +325,7 @@ for (mut in all_who_mutations){
   # --- Crop dataframes to bbox ------------------------------
   p_long_median   <- crop_long_df(p_long_median, xlim, ylim)
   p_long_mean   <- crop_long_df(p_long_mean, xlim, ylim)
+  p_long_CI   <- crop_long_df(p_long_CI, xlim, ylim)
   
   exceed_prob_long_10 <- crop_long_df(exceed_prob_long_10, xlim, ylim)
   exceed_prob_long_5  <- crop_long_df(exceed_prob_long_5,  xlim, ylim)
@@ -338,14 +348,15 @@ for (mut in all_who_mutations){
     )
   
   # --- Average exceed prob and pred prev every two years --------------------
-  p_long_median_avg_2y <- avg_2_year(p_long_median)
-  p_long_mean_avg_2y <- avg_2_year(p_long_mean)
+  p_long_median_avg_2y <- avg_2_year(p_long_median %>% filter(t %in% plot_times))
+  p_long_mean_avg_2y <- avg_2_year(p_long_mean %>% filter(t %in% plot_times))
+  p_long_CI_avg_2y <- avg_2_year(p_long_CI %>% filter(t %in% plot_times))
   
-  exceed_prob_long_1_avg_2y <- avg_2_year(exceed_prob_long_1)
-  exceed_prob_long_5_avg_2y <- avg_2_year(exceed_prob_long_5)
-  exceed_prob_long_10_avg_2y <- avg_2_year(exceed_prob_long_10)
+  exceed_prob_long_1_avg_2y <- avg_2_year(exceed_prob_long_1 %>% filter(t %in% plot_times))
+  exceed_prob_long_5_avg_2y <- avg_2_year(exceed_prob_long_5 %>% filter(t %in% plot_times))
+  exceed_prob_long_10_avg_2y <- avg_2_year(exceed_prob_long_10 %>% filter(t %in% plot_times))
   
-  points_df_2y <- add_year_block(points_df)
+  points_df_2y <- add_year_block(points_df %>% filter(t %in% plot_times))
   
   # --- Helper to generate a consistent ggplot with point overlays ------------
   plot_theme <- function(p) {
@@ -456,15 +467,21 @@ for (mut in all_who_mutations){
   plot_mean <- plot_layer(p_long_mean_avg_2y, clean_mut, shape_Africa_crop, shape_water_crop, add_points = TRUE, add_legend = TRUE)
   plot_mean_no_points  <- plot_layer(p_long_mean_avg_2y, clean_mut, shape_Africa, shape_water, add_points = FALSE, add_legend = TRUE)
   
+  plot_CI <- plot_layer(p_long_CI_avg_2y, clean_mut, shape_Africa_crop, shape_water_crop, add_points = TRUE, add_legend = TRUE)
+  plot_CI_no_points <- plot_layer(p_long_CI_avg_2y, clean_mut, shape_Africa_crop, shape_water_crop, add_points = FALSE, add_legend = TRUE)
+  
   plot_exceed_1 <- plot_exceedance(exceed_prob_long_1_avg_2y, title_text = clean_mut, legend_title =expression(Pr(Prevalence >= 1*"%")))
   plot_exceed_5 <- plot_exceedance(exceed_prob_long_5_avg_2y, title_text = clean_mut, legend_title =expression(Pr(Prevalence >= 5*"%")))
   plot_exceed_10 <- plot_exceedance(exceed_prob_long_10_avg_2y, title_text = clean_mut, legend_title =expression(Pr(Prevalence >= 10*"%")))
-
-  save_figs(file.path(OUT_PREV_DIR, paste0(mut, "_mean_perc_prev")), plot_mean, width = 10)
-  save_figs(file.path(OUT_PREV_DIR, paste0(mut, "_mean_perc_prev_no_points")), plot_mean_no_points, width = 10)
   
-  save_figs(file.path(OUT_PREV_DIR, paste0(mut, "_median_perc_prev")), plot_median, width = 10)
-  save_figs(file.path(OUT_PREV_DIR, paste0(mut, "_median_perc_prev_no_points")), plot_median_no_points, width = 10)
+  save_figs(file.path(OUT_MEAN_DIR, paste0(mut, "_mean_perc_prev")), plot_mean, width = 10)
+  save_figs(file.path(OUT_MEAN_DIR, paste0(mut, "_mean_perc_prev_no_points")), plot_mean_no_points, width = 10)
+  
+  save_figs(file.path(OUT_MEDIAN_DIR, paste0(mut, "_median_perc_prev")), plot_median, width = 10)
+  save_figs(file.path(OUT_MEDIAN_DIR, paste0(mut, "_median_perc_prev_no_points")), plot_median_no_points, width = 10)
+  
+  save_figs(file.path(OUT_CI_DIR, paste0(mut, "_CI_diff_prev")), plot_CI, width = 10)
+  save_figs(file.path(OUT_CI_DIR, paste0(mut, "_CI_diff_prev_no_points")), plot_CI_no_points, width = 10)
 
   save_figs(file.path(OUT_EXCEED_DIR, paste0(mut, "_exceednace_prob_1")), plot_exceed_1, width = 10)
   save_figs(file.path(OUT_EXCEED_DIR, paste0(mut, "_exceednace_prob_5")), plot_exceed_5, width = 10)
